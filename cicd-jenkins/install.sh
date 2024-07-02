5e63892c4ee6ef251dbf882b9239715ca5264523
beadeveloper9789 <148839853+beadeveloper9789@users.noreply.github.com>
2024-07-02 22:18:43 +0530

    Create install.sh

---
+++ cicd-jenkins/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Update the package manager and install Docker
+sudo apt-get update -y
+sudo apt-get install -y docker.io
+
+# Start the Docker service
+sudo systemctl start docker
+
+# Enable Docker to start on boot
+sudo systemctl enable docker
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+sudo docker run -d --name netflix -p 82:80 naresh345/phpapplication:latest
